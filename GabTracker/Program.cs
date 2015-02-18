89005d7d2afd6f6346353a0a4a2844cdc2210704
Gabriel <gabrielmongeon@gmail.com>
2015-02-17 21:12:46 -0500

    Cleaning

--- GabTracker/Program.cs
+++ GabTracker/Program.cs
@@ -190,7 +190,7 @@ namespace GabTracker
             }
             else
             {
-                SetSDActivated();
+                SetSDActivated();                
             }
 
         }
