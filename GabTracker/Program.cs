5a015266523ffae489b01f43453d8538fbe60db5
Gabriel <gabrielmongeon@gmail.com>
2015-02-20 09:45:13 -0500

    Automatically On & cleanup
    I've switch the start up state as ready to write to the USB since it's
    way better for unmonitored testing

--- GabTracker/Program.cs
+++ GabTracker/Program.cs
@@ -17,38 +17,25 @@ namespace GabTracker
         double _temperature = -10000;
         double _humidity = -10000;
         private StorageDevice _storage;
-        private bool _SDActivated = false;
-        private static string _dataFilePath = @"\USB\text.txt";// @"\SD\AirQuality.TXT";
+        private bool _SDActivated = true;
+        private static string _dataFilePath = @"\USB\GabTracker.csv";
         private TimeSpan _blinkInterval = new TimeSpan(0, 0, 0, 0, 100);
 
-        private static AutoResetEvent evt = new AutoResetEvent(false);
-
-        // This method is run when the mainboard is powered up or reset.   
+        /// <summary>
+        /// This method is run when the mainboard is powered up or reset.
+        /// </summary>
         void ProgramStarted()
         {
-            /*******************************************************************************************
-            Modules added in the Program.gadgeteer designer view are used by typing 
-            their name followed by a period, e.g.  button.  or  camera.
-            
-            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
-                button.ButtonPressed +=<tab><tab>
-            
-            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
-                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
-                timer.Tick +=<tab><tab>
-                timer.Start();
-            *******************************************************************************************/
             Debug.Print("Setting up...");
 
             _timer = new Gadgeteer.Timer(5000, Gadgeteer.Timer.BehaviorType.RunContinuously);
             _timer.Tick += _timer_Tick;
 
-            btnSDToggle.TurnLedOn();
+            btnSDToggle.TurnLedOff();
             btnSDToggle.Mode = Gadgeteer.Modules.GHIElectronics.Button.LedMode.ToggleWhenReleased;
             btnSDToggle.ButtonReleased += btnSDToggle_ButtonReleased;
 
             gps.InvalidPositionReceived += gps_InvalidPositionReceived;
-            gps.NmeaSentenceReceived += gps_NmeaSentenceReceived;
             gps.PositionReceived += gps_PositionReceived;
             gps.DebugPrintEnabled = true;
 
@@ -63,13 +50,14 @@ namespace GabTracker
             {
                 _storage = usbHost.MassStorageDevice;
             }
-            SetSDActivated();
-            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
-            Debug.Print("Program Started");
+            SetSDActivated(true);
+
+            Debug.Print("Program Starting...");
 
             gps.Enabled = true;
             _timer.Start();
             tempHumidity.StartTakingMeasurements();
+            Debug.Print("Program Started");
         }
 
         void usbHost_MassStorageUnmounted(USBHost sender, EventArgs e)
@@ -82,21 +70,14 @@ namespace GabTracker
             _storage = device;
         }
 
-
-        void RemovableMedia_Insert(object sender, MediaEventArgs e)
-        {
-            Debug.Print("Inserted");
-            evt.Set();
-        }
-
         void btnSDToggle_ButtonReleased(Gadgeteer.Modules.GHIElectronics.Button sender, Gadgeteer.Modules.GHIElectronics.Button.ButtonState state)
         {
-            _SDActivated = !btnSDToggle.IsLedOn;
-            SetSDActivated();
+            SetSDActivated(!btnSDToggle.IsLedOn);
         }
 
-        private void SetSDActivated()
+        private void SetSDActivated(bool value)
         {
+            _SDActivated = value;
             if (_SDActivated)
             {
                 ledSDActivated.TurnColor(Color.Green);
@@ -106,23 +87,6 @@ namespace GabTracker
                 ledSDActivated.TurnColor(Color.Red);
             }
         }
-
-        //void sdCard_Mounted(SDCard sender, StorageDevice device)
-        //{
-        //    PulseDebugLED();
-        //    if (sdCard.IsCardMounted)
-        //    {
-        //        _storage = sdCard.StorageDevice;
-        //    }
-        //}
-
-        //void sdCard_Unmounted(SDCard sender, EventArgs e)
-        //{
-        //    PulseDebugLED();
-        //    _storage = null;
-        //}
-
-
 
         void tempHumidity_MeasurementComplete(TempHumidity sender, TempHumidity.MeasurementCompleteEventArgs e)
         {
@@ -157,11 +121,6 @@ namespace GabTracker
             Utility.SetLocalTime(e.FixTimeUtc);
         }
 
-        void gps_NmeaSentenceReceived(GPS sender, string e)
-        {
-            //   Debug.Print("NMEA sentence: " + e);
-        }
-
         void gps_InvalidPositionReceived(GPS sender, EventArgs e)
         {
             Debug.Print("Invalid Position Received");
@@ -193,12 +152,7 @@ namespace GabTracker
                     string fullRecord = recordPosition + ";"
                         + tempHumidityRecord;
 
-
                     var files = _storage.ListFiles(_storage.RootDirectory);
-                    //var sw = new StreamWriter(File.Open(_dataFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite));
-                    //sw.WriteLine(fullRecord);
-                    //sw.Flush();
-                    //sw.Close();
 
                     using (var fs = new FileStream(_dataFilePath, FileMode.Append))
                     {
@@ -207,6 +161,7 @@ namespace GabTracker
                         fs.Flush();
                         fs.Close();
                     }
+
                     _storage.Volume.FlushAll();
 
                     Debug.Print("Local time: " + DateTime.Now + " ---- " + DateTime.UtcNow);
@@ -218,12 +173,12 @@ namespace GabTracker
                 }
                 finally
                 {
-                    SetSDActivated();
+                    SetSDActivated(_SDActivated);
                 }
             }
             else
             {
-                SetSDActivated();
+                SetSDActivated(false);
             }
 
         }
